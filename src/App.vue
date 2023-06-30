eb91a2d7d012534e9af51d01c4a231362ffad0bf
mervys <merveilleagbogan@gmail.com>
2023-06-30 12:52:46 +0100

    add tailwind to the projects

--- src/App.vue
+++ src/App.vue
@@ -3,28 +3,24 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="">
+    <div class="bg-white w-100 py-4 flex justify-center"  >
+<img src="/src/assets/images/logo.png" alt="">
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div class="bg-white flex justify-center items-center w-[509px] h-[394px]">
+  </div>
+
+  </div>
+
+  
+    <!-- <h1>Créer Votre Compte</h1>
+    <p>Rejoignez l'entrepise Open SI SAS sur la plateforme de suivi des reversements Payout Tracker en tant que : Financier</p>
+    <label for=""></label> -->
+    
+ 
+
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style>
+
 </style>
