ef45a9ed4254b0760e9a6fd8d2add8275468e3ef
mervys <merveilleagbogan@gmail.com>
2023-07-04 10:57:11 +0100

    first page

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,152 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+<!--<script setup lang="ts">
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
   <div class="">
-    <div class="bg-white w-100 py-4 flex justify-center"  >
-<img src="/src/assets/images/logo.png" alt="">
+    <div class="fixed bg-white w-screen py-4 flex justify-center">
+      <img src="/src/assets/images/logo.png" alt="" />
+    </div>
+    <div class="flex justify-center items-center h-screen">
+      <div class="bg-white w-[510px] h-80">
+        <h1 class="text-blue-950 flex justify-center font-bold py-3">
+          Me connecter
+        </h1>
+
+        <form>
+          <div>
+          <label class="block px-10 w-96 h-96">
+            <span class="block text-sm py-3">Adresse Mail</span>
+            <input
+              type="Adresse Mail"
+              class="rounded border-2 border-indigo-500/50 w-[402px] h-10"
+            />
+            <span class="block text-sm py-3">Mot de passe</span>
+            <div class="relative w-[402px] h-10">
+              <input
+                type="Mot de passe "
+                class="rounded border-2 border-indigo-500/50 h-10 w-full"
+              />
+              <div class="absolute right-0 inset-y-0 flex items-center mr-2">
+                <img
+                  src="/src/assets/images/Vector.png"
+                  
+                  alt=""/>
+              </div>
+            </div>
+
+            <span class="block py-3"></span>
+            <div class="rounded border-2 bg-red-500 h-10 w-[402px] flex justify-center">
+          
+            <div class=" flex justify-center items-center text-white py-3">
+          Se Connecter
+        </div>
+            </div>
+          </label>
+        </div>
+        </form>
+      </div>
+    </div>
+    <div>
+    <a href="Mot de passe oublié ?" class="text-blue-600 flex justify-center underline hover:underline-offset-4">
+      Mot de passe oublié ?
+    </a>
   </div>
-  <div class="bg-white flex justify-center items-center w-[509px] h-[394px]">
   </div>
 
-  </div>
 
   
-    <!-- <h1>Créer Votre Compte</h1>
-    <p>Rejoignez l'entrepise Open SI SAS sur la plateforme de suivi des reversements Payout Tracker en tant que : Financier</p>
-    <label for=""></label> -->
-    
- 
-
+  
 </template>
 
-<style>
+<style></style>-->
 
-</style>
+
+<template>
+
+<div class="">
+    <div class="fixed bg-white w-screen py-4 flex justify-center">
+      <img src="/src/assets/images/logo.png" alt="" />
+    </div>
+    <div class=" flex justify-center items-center h-screen">
+      <div class="bg-white w-96 h-80 rounded border-2">
+        <h1 class=" flex justify-center font-bold py-3">
+        Votre lien d'invitation a expiré
+        </h1>
+        <div class="flex justify-center py-4  ">
+        <img src="/src/assets/images/Frame 1.png" alt="" />
+        </div>
+        <div class="flex justify-center py-4 px-10">
+          <h1>Contacter votre référent pour obtenir un nouveau lien d’invitation</h1>
+        </div>
+      </div>
+      </div>
+    </div>
+</template>-->
+
+<!--<template>
+
+<div class="">
+    <div class="fixed bg-white w-screen py-4 flex justify-center">
+      <img src="/src/assets/images/logo.png" alt="" />
+    </div>
+    <div class=" flex justify-center items-center h-screen">
+      <div class="bg-white w-[514px] h-96 rounded border-2">
+        <div class="flex justify-center">
+      <img src="/src/assets/images/Main.png" alt="" />
+    </div>
+    <div class="flex justify-center py-10">
+      <img src="/src/assets/images/mail-illustration.svg" alt="" />
+      
+    </div>
+    <h1 class=" flex justify-center font-bold text-blue-950 py-3">
+        Consulter Vos mails
+        </h1>
+        <P class="flex justify-center text-center">
+          Nous avons envoyé un lien de vérification à votre adresse mail :
+          
+        </P>
+        <a href="jean@yopmail.com" class="text-blue-950 flex justify-center">jean@yopmail.com
+      
+    </a>
+        <p class="flex justify-center py-4">
+        Vous n’avez pas reçu le code ? 
+        <a href="Renvoyer le code" class="text-blue-600 flex justify-center">Renvoyer le code
+      
+    </a>
+      </p>
+      
+      </div>
+    </div>
+  </div>
+
+</template>-->
+
+
+
+<!--<template>
+  <div class="">
+    <div class="fixed bg-white w-screen py-4 flex justify-center">
+      <img src="/src/assets/images/logo.png" alt="" />
+    </div>
+    <div class="flex justify-center items-center h-screen">
+      <div class="bg-white w-[508px] h-80">
+        <div class="flex justify-center py-6">
+        <img src="/src/assets/images/Group 356.png"  alt="" /></div>
+        <h1 class="text-blue-950 flex justify-center text-center font-bold  py-1">
+          Félicitations ! Votre mot de passe a été modifié avec succès !
+        </h1>
+        <p class="flex justify-center text-center">Vous pouvez vous connecter afin de rejoindre l’espace de travail de votre entreprise !</p>
+
+            <span class="block py-3"></span>
+            <div class="rounded border-2 bg-red-500 h-10 w-[402px] flex justify-center ">
+          
+            <div class=" flex justify-center items-center text-white py-3">
+          Se Connecter
+        </div>
+      </div>
+            </div>
+      </div>
+    </div>
+    
+</template>-->
---
+++ src/components/fist.vue
@@ -0,0 +1,16 @@
+<template>
+
+<div class="">
+    <div class="fixed rounded border-2 bg-white w-screen py-4 flex justify-center">
+      <img src="/src/assets/images/Frame 1.png" alt="" />
+    </div>
+    <div class="flex justify-center items-center h-screen">
+      <div class="bg-white w-[405px] h-86">
+        <h1 class="text-blue-950 flex justify-center font-bold py-3">
+        Contacter votre référent pour obtenir un nouveau lien d'invitation
+        </h1>
+
+        
+      </div>
+    </div>
+</template>
