9bae152b8c74673a13a3b364b5cf2f8c5e58ba09
mervys <merveilleagbogan@gmail.com>
2023-07-04 16:40:22 +0100

    create input

---
+++ src/components/VInput.vue
@@ -0,0 +1,13 @@
+<template>
+  <label class="block px-10 w-96 h-96">
+    <span class="block text-sm py-3">{{ label }}</span>
+    <input
+      :type="type"
+      class="rounded border-2 border-indigo-500/50 w-[402px] h-10"
+    />
+  </label>
+</template>
+
+<script setup lang="ts">
+defineProps<{ type: string; label: string }>();
+</script>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,17 +1,19 @@
 <script setup lang="ts">
+import VInput from "../components/VInput.vue";
 import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-<div>
-  <div class="bg-white w-[510px] h-80">
-        <h1 class="text-blue-950 flex justify-center font-bold py-3">
-          Me connecter
-        </h1>
+  <div>
+    <div class="bg-white w-[510px] h-80">
+      <h1 class="text-blue-950 flex justify-center font-bold py-3">
+        Me connecter
+      </h1>
 
-        <form>
-          <div>
-          <label class="block px-10 w-96 h-96">
+      <form>
+        <div>
+          <VInput type="text" label="Adresse Mail" />
+          <!-- <label class="block px-10 w-96 h-96">
             <span class="block text-sm py-3">Adresse Mail</span>
             <input
               type="Adresse Mail"
@@ -24,30 +26,29 @@ import HelloWorld from "./components/HelloWorld.vue";
                 class="rounded border-2 border-indigo-500/50 h-10 w-full"
               />
               <div class="absolute right-0 inset-y-0 flex items-center mr-2">
-                <img
-                  src="/src/assets/images/Vector.png"
-                  
-                  alt=""/>
+                <img src="/src/assets/images/Vector.png" alt="" />
               </div>
             </div>
 
             <span class="block py-3"></span>
-            <div class="rounded border-2 bg-red-500 h-10 w-[402px] flex justify-center">
-          
-            <div class=" flex justify-center items-center text-white py-3">
-          Se Connecter
-        </div>
+            <div
+              class="rounded border-2 bg-red-500 h-10 w-[402px] flex justify-center"
+            >
+              <div class="flex justify-center items-center text-white py-3">
+                Se Connecter
+              </div>
             </div>
-          </label>
+          </label> -->
         </div>
-        </form>
-      </div>
-      <a href="Mot de passe oublié ?" class="text-blue-600 flex justify-center underline hover:underline-offset-4">
+      </form>
+    </div>
+    <a
+      href="Mot de passe oublié ?"
+      class="text-blue-600 flex justify-center underline hover:underline-offset-4"
+    >
       Mot de passe oublié ?
     </a>
-</div>
-     
-
+  </div>
 </template>
 
 <style></style>
